e7f08873872e2012c0109a1577b3ba280b46c604
kartik251 <71187585+kartik251@users.noreply.github.com>
2025-01-21 19:41:41 +0530

    Create drill_down.sql

---
+++ sql_queries/drill_down.sql
@@ -0,0 +1,8 @@
+-- 7. Drill-down: Top Selling Products per Region
+SELECT 
+    Region, 
+    ProductName, 
+    SUM(TransactionAmount) AS TotalRevenue
+FROM sales_data
+GROUP BY Region, ProductName
+ORDER BY Region, TotalRevenue DESC;
