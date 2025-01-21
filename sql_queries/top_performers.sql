1cc1da96bdf8d4315c487fe13238def0235f98cf
kartik251 <71187585+kartik251@users.noreply.github.com>
2025-01-21 19:40:13 +0530

    Create top_performers.sql

---
+++ sql_queries/top_performers.sql
@@ -0,0 +1,24 @@
+-- 2. Revenue by Product Category
+SELECT 
+    ProductName, 
+    SUM(TransactionAmount) AS TotalRevenue
+FROM sales_data
+GROUP BY ProductName
+ORDER BY TotalRevenue DESC
+LIMIT 10;
+
+-- 3. Top Performing Regions by Revenue
+SELECT 
+    Region, 
+    SUM(TransactionAmount) AS TotalRevenue
+FROM sales_data
+GROUP BY Region
+ORDER BY TotalRevenue DESC;
+
+-- 4. Identifying Seasonal Patterns (Monthly Sales Trend)
+SELECT 
+    DATE_TRUNC('month', TransactionDate) AS Month,
+    SUM(TransactionAmount) AS TotalRevenue
+FROM sales_data
+GROUP BY Month
+ORDER BY Month;
