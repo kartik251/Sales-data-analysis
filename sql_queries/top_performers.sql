e02a83ca564d147135f6b36d54bc6fe8342d6f79
kartik251 <71187585+kartik251@users.noreply.github.com>
2025-01-21 21:09:26 +0530

    Update top_performers.sql

--- sql_queries/top_performers.sql
+++ sql_queries/top_performers.sql
@@ -7,7 +7,7 @@ GROUP BY ProductName
 ORDER BY TotalRevenue DESC
 LIMIT 10;
 
--- 3. Top Performing Regions by Revenue
+-- Top Performing Regions by Revenue
 SELECT 
     Region, 
     SUM(TransactionAmount) AS TotalRevenue
@@ -15,7 +15,7 @@ FROM sales_data
 GROUP BY Region
 ORDER BY TotalRevenue DESC;
 
--- 4. Identifying Seasonal Patterns (Monthly Sales Trend)
+-- Identifying Seasonal Patterns (Monthly Sales Trend)
 SELECT 
     DATE_TRUNC('month', TransactionDate) AS Month,
     SUM(TransactionAmount) AS TotalRevenue
@@ -23,7 +23,7 @@ FROM sales_data
 GROUP BY Month
 ORDER BY Month;
 
--- 5. High-Value Customers (Top 10 Customers by Revenue)
+-- High-Value Customers (Top 10 Customers by Revenue)
 SELECT 
     CustomerID, 
     SUM(TransactionAmount) AS TotalSpent
